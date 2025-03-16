8f1d9ce92d250abdf0a00e1758c279146a2f4da8
Justin Purnell <jpurnell@users.noreply.github.com>
2025-03-16 16:11:53 -0400

    Schedule Preview for Jack

--- Sources/Helpers/Schedule Page Helpers/eventRow.swift
+++ Sources/Helpers/Schedule Page Helpers/eventRow.swift
@@ -8,6 +8,35 @@
 import Foundation
 import Ignite
 
+//func eventRow(_ events: [Event], day value: Int) -> [Row] {
+//	var rows: [Row] = []
+//	for event in events.filter({$0.startComponents.weekday == value}) {
+//		let row =
+//			Row {
+////				Image(systemName: eventTypeIcon(event.type).0).foregroundStyle(.princetonOrange)
+////				Text("\(eventTypeIcon(event.type).1)").horizontalAlignment(.center)
+//				Text(event.eventStartEnd)/*.frame(width: 160, alignment: .leading)*/
+////				if let _ = event.description {
+////					Accordion {
+////						Item(event.name) {
+////							Text(event.description!)
+////						}
+////					}
+////				} else {
+////					Text(event.name)
+////				}
+//				Text(event.name)/*.fontWeight(.medium).frame(width: 200, alignment: .leading)*/
+//				Text(event.primaryLocation)/*.frame(width: 200, alignment: .leading)*/
+//			}
+//		rows.append(row)
+//		}
+//	return rows
+//}
+
+
+import Foundation
+import Ignite
+
 func eventRow(_ events: [Event], day value: Int) -> [Row] {
 	var rows: [Row] = []
 	for event in events.filter({$0.startComponents.weekday == value}) {
@@ -16,20 +45,10 @@ func eventRow(_ events: [Event], day value: Int) -> [Row] {
 //				Image(systemName: eventTypeIcon(event.type).0).foregroundStyle(.princetonOrange)
 				Text("\(eventTypeIcon(event.type).1)").horizontalAlignment(.center)
 				Text(event.eventStartEnd)/*.frame(width: 160, alignment: .leading)*/
-//				if let _ = event.description {
-//					Accordion {
-//						Item(event.name) {
-//							Text(event.description!)
-//						}
-//					}
-//				} else {
-//					Text(event.name)
-//				}
 				Text(event.name)/*.fontWeight(.medium).frame(width: 200, alignment: .leading)*/
-				Text(event.location)/*.frame(width: 200, alignment: .leading)*/
+				Text(event.primaryLocation)/*.frame(width: 200, alignment: .leading)*/
 			}
 		rows.append(row)
 		}
 	return rows
 }
-
