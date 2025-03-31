adbe594c98c2d6ba15da09d762629e2cc6081d16
Justin Purnell <jpurnell@users.noreply.github.com>
2025-03-31 09:31:20 -0400

    Add Crew Page

---
+++ Sources/Helpers/Schedule Page Helpers/CommitteeMember.swift
@@ -0,0 +1,16 @@
+//
+//  Committee.swift
+//  IgniteStarter
+//
+//  Created by Justin Purnell on 10/15/24.
+//
+
+import Foundation
+
+struct CommitteeMember: Codable {
+	let chair: String
+	let role: String
+	let photo: String
+	let email: String
+	let priority: Int
+}
---
+++ Sources/Pages/Crew.swift
@@ -0,0 +1,35 @@
+//
+//  Housing.swift
+//
+//
+//  Created by Justin Purnell on 7/8/24.
+//
+
+import Foundation
+import Ignite
+
+struct Committee: StaticPage {
+	var title = "Committee"
+	var description: String = "Your incredible Reunions Committee"
+	var image: String? = "/images/logos/P2000_25th_Lounging_Tiger.svg"
+
+	func body(context: PublishingContext) -> [BlockElement] {
+		var committee: [CommitteeMember] { let decoder = JSONDecoder(); let data = try! Data(contentsOf: URL(fileURLWithPath: "/Users/jpurnell/Dropbox/Computer/Development/Swift/Princeton/Website/ExampleSite/Resources/committee.json")); let committeeMembers = try? decoder.decode([CommitteeMember].self, from: data); return committeeMembers?.sorted(by: {$0.priority < $1.priority && $0.role < $1.role}) ?? []}
+		
+		Table {
+			for members in committee.batched(into: 3) {
+				Row {
+					for member in members {
+						Column {
+							member.photo == "" ?
+								Image.tiger.resizable().frame(width: 100, height: 100) :
+								Image(member.photo).resizable().frame(width: 100, height: 100)
+							Text(member.chair).fontWeight(.semibold)
+							Text(member.role)
+						}.horizontalAlignment(.center)
+					}
+				}
+			}
+		}
+	}
+}
